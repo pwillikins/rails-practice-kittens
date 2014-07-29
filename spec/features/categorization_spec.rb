ffde8b671f2b306e7417d2d9076fac305906f285
Paul Stone <pmstone247@gmail.com>
2014-07-29 14:23:04 -0600

    cannot assign duplicate categorizations

--- spec/features/categorization_spec.rb
+++ spec/features/categorization_spec.rb
@@ -11,6 +11,20 @@ feature "validating categories" do
     expect(page).to have_content("Category can't be blank")
   end
 
+  scenario "kitten cannot have duplicate categorizations" do
+    user = create_user email: "user@example.com"
+    Category.create!(name: "Cutest!")
+    Kitten.create!(image: "http://i.imgur.com/tOzb0dUb.jpg")
+    login(user)
+    find(".kitten-link").click
+    select "Cutest!", from: "categorization_category_id"
+    click_on "Add Category"
+    find(".kitten-link").click
+    select "Cutest!", from: "categorization_category_id"
+    click_on "Add Category"
+    expect(page).to have_content("Category has already been taken")
+  end
+
   def login(user)
     visit root_path
     click_on "Login"
